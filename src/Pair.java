e1e53caea2d1082a36d71f00f9abd2b8470c6b8e
TomaszAgent <114474578+TomaszAgent@users.noreply.github.com>
2024-03-30 16:47:04 +0100

    Change Pair to record

--- src/Pair.java
+++ src/Pair.java
@@ -1,17 +1,2 @@
-public class Pair<T1, T2> {
-    private final T1 first;
-    private final T2 second;
-
-    public Pair(T1 first, T2 second){
-        this.first = first;
-        this.second = second;
-    }
-
-    public T1 getFirst() {
-        return first;
-    }
-
-    public T2 getSecond() {
-        return second;
-    }
+public record Pair<T1, T2>(T1 first, T2 second) {
 }
