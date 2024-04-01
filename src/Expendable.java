afc0431da2f9328e6d9e2877d8c28758daf1ae58
TomaszAgent <114474578+TomaszAgent@users.noreply.github.com>
2024-04-01 13:37:09 +0200

    Fix Expandable

--- src/Expendable.java
+++ src/Expendable.java
@@ -1,4 +1,8 @@
 public interface Expendable {
-    public boolean buildHouse();
-    public boolean buildHotel();
+    public boolean buildHouse(Player player, int amount);
+    public boolean buildHotel(Player player);
+    public int getHousePrice();
+    public int getHotelPrice();
+    public boolean sellHouse(Player player, int amount);
+    public boolean sellHotel(Player player);
 }
