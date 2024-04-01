a7fb43931a1d83ace22751d765f2969787f498ac
TomaszAgent <114474578+TomaszAgent@users.noreply.github.com>
2024-04-01 16:33:40 +0200

    Update buyable

--- src/Buyable.java
+++ src/Buyable.java
@@ -4,6 +4,6 @@ public interface Buyable {
     public int getMortgagePrice();
     public boolean buy(Player player);
     public boolean buy(Player player, int price);
-    public boolean mortgage();
-    public boolean payMortgage();
+    public boolean mortgage(Player player);
+    public boolean payMortgage(Player player);
 }
