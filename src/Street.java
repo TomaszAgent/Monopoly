dbfe90358f7f29d8e4cebb7c950926406c88ffd8
TomaszAgent <114474578+TomaszAgent@users.noreply.github.com>
2024-04-01 16:20:09 +0200

    Update Street

--- src/Street.java
+++ src/Street.java
@@ -6,15 +6,21 @@ public class Street extends Field implements Buyable, Expendable{
     private final int hotelPrice;
     private Player owner;
     private final ImmutableMap<Integer, Integer> rent;
+    private final String colour;
+    private boolean mortgaged;
+    private final int mortgagePrice;
 
-    public Street(String name, int price, int housePrice, int hotelPrice, ImmutableMap<Integer, Integer> rent){
+    public Street(String name, int price, int housePrice, int hotelPrice, ImmutableMap<Integer, Integer> rent, String colour, int mortgagePrice){
         this.name = name;
         this.price = price;
         this.housePrice = housePrice;
         this.hotelPrice = hotelPrice;
         this.rent = rent;
+        this.colour = colour;
+        this.mortgagePrice = mortgagePrice;
         houses = 0;
         owner = null;
+        mortgaged = false;
     }
 
     @Override
@@ -48,12 +54,61 @@ public class Street extends Field implements Buyable, Expendable{
         return rent;
     }
 
+    public String getColour() {
+        return colour;
+    }
+
+    public boolean isMortgaged() {
+        return mortgaged;
+    }
+
+    public int getMortgagePrice() {
+        return mortgagePrice;
+    }
+
     public boolean buy(Player player){
         if(owner == null || player.getMoney() < price){
             return false;
         }
         player.subtractMoney(price);
         owner = player;
+        return true;
+    }
+
+    public boolean buy(Player player, int price){
+        if(owner == null || player.getMoney() < price){
+            return false;
+        }
+        player.subtractMoney(price);
+        owner = player;
+        return true;
+    }
+
+    public boolean sell(Player player, int price){
+        if(player.getMoney() < price){
+            return false;
+        }
+        player.subtractMoney(price);
+        owner.addMoney(price);
+        owner = player;
+        return true;
+    }
+
+    public boolean mortgage(Player player){
+        if(player != owner || houses != 0){
+            return false;
+        }
+        owner.addMoney(mortgagePrice);
+        mortgaged = true;
+        return true;
+    }
+
+    public boolean payMortgage(Player player){
+        if(player != owner || player.getMoney() < mortgagePrice * 1.1){
+            return false;
+        }
+        player.subtractMoney((int) (mortgagePrice * 1.1));
+        mortgaged = false;
         return true;
     }
 
@@ -97,12 +152,21 @@ public class Street extends Field implements Buyable, Expendable{
         return true;
     }
 
-    public boolean payRent(Player player){
-        if(owner == null || rent.get(houses) > player.getMoney()){
+    public boolean payRent(Player player, ImmutableMap<String, Neighbourhood> neighbourhoods){
+        int toPay = rent.get(houses);
+        ThreePair<Street, Street, Street> neighbourhoodStreets = neighbourhoods.get(colour).streets();
+        if(
+                neighbourhoodStreets.first().getOwner() == owner &&
+                neighbourhoodStreets.second().getOwner() == owner &&
+                neighbourhoodStreets.third().getOwner() == owner
+        ){
+            toPay *= 2;
+        }
+        if(owner == null || toPay > player.getMoney()){
             return false;
         }
-        player.subtractMoney(rent.get(houses));
-        owner.addMoney(rent.get(houses));
+        player.subtractMoney(toPay);
+        owner.addMoney(toPay);
         return true;
     }
 }
